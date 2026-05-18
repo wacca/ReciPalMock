a81d0638deeefcb4fccc9b11ed1970ece12d606c
wakabayashi <h_wakabayashi@ubicast.com>
2026-05-18 21:38:32 +0900

    Updates

---
+++ assets/Edit-B3lAWlas.js
@@ -0,0 +1,1 @@
+import{W as e,bt as t}from"./index-B-WlDOS4.js";var n=e((0,t().jsx)(`path`,{d:`M3 17.25V21h3.75L17.81 9.94l-3.75-3.75zM20.71 7.04c.39-.39.39-1.02 0-1.41l-2.34-2.34a.996.996 0 0 0-1.41 0l-1.83 1.83 3.75 3.75z`}),`Edit`);export{n as t};
